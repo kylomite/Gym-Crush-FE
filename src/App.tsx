597ef6a838960ce838302f34354affc97aa1b602
Kyle Delaney <khdelaney99@gmail.com>
2024-12-03 09:04:38 -0500

    install tailwind and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
 import HomePage from './HomePage/HomePage.tsx';
 import LoginSignUpPage from "./LoginSignUpPage/LoginSignUpPage.tsx";
+import Logo from './assets/gymCrushLogo.png'
 
 function App() {
   return <main className='app'>
     <Link className="logo" to={'/'}>
-      <h1 >Gym Crush</h1>
+      <img className="logo w-32 h-32 object-contain" 
+      src={Logo} 
+      alt="Gym Crush Logo" />
     </Link>
     <Routes>
       <Route path="/" element={<HomePage />}/>
